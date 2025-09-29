2090f25ab68a5ed75e96ef235326bee051fa1537
gibral0038 <gibraldizilal@gmail.com>
2025-09-29 10:25:09 +0700

    Day 3

---
+++ Mencetak Faktorisasi.cpp
@@ -0,0 +1,30 @@
+/*
+Tanggal     : 
+Deskripsi   : 
+Waktu       : 
+*/ 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void faktor(int n){
+    for (int a = 2; a <= n; a++){
+        int temp = 1;
+        if (n % a == 0){
+            n /= a;
+            while (n % a == 0){
+                temp++;
+                n /= a;
+            }
+            cout << a;
+            if (temp > 1) cout << "^" << temp;
+            if (n > 1) cout << " x "; 
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    faktor(n);
+}
